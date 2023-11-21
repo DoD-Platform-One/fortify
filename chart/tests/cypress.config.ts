acd57fc6a838c9badc7e3124cf0b8e9f3a857327
Jared Ladner <jared.ladner@sev1tech.com>
2023-11-21 16:05:56 +0000

    disabling migration as it takes too long during installs

--- chart/tests/cypress.config.ts
+++ chart/tests/cypress.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    videoCompression: false,
     env: {
       url: "https://fortify.bigbang.dev",
       new_pwd: "6g#yN2^Ky[l^kPA"
     },
+    video: true,
+    videoCompression: 35,
+    screenshot: true,
+    screenshotOnRunFailure: true,
     supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
